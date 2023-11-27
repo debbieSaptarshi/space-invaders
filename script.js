61eaad2dff0c136f96c6ee6e8852a8aeacc6630f
debbieSaptarshi <148401605+debbieSaptarshi@users.noreply.github.com>
2023-11-27 23:19:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -240,7 +240,7 @@ class Boss {
         this.height = 200;
         this.x = this.game.width * 0.5 - this.width * 0.5;
         this.y = -this.height;
-        this.speedX = Math.random() < 0.5 ? -1 : 1;
+        this.speedX = Math.random() < 0.5 ? -1 : 5;
         this.speedY = 0;
         this.lives = bossLives;
         this.maxLives = this.lives;
@@ -310,7 +310,7 @@ class Wave {
         this.height = this.game.rows * this.game.enemySize;
         this.x = this.game.width * 0.5 - this.width * 0.5;
         this.y = -this.height;
-        this.speedX = Math.random() < 0.5 ? -1 : 1;
+        this.speedX = Math.random() < 0.5 ? -1 : 5;
         this.speedY = 0;
         this.enemies = [];
         this.nextWaveTrigger = false;
